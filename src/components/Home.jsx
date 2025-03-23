4224c8b9ab0112e7a6061c5f2a8c4c5feea8f6bd
Nick Zheng <nickzheng81@gmail.com>
2025-03-23 13:01:45 -0400

    added changing phrases to home page animation

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,33 @@
 import React from "react";
-import { useEffect } from "react";
-import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
 import "./css/home.css";
 
 
 const Home = () => {
+
+  const phrases = [
+    "Nick Zheng",
+    "a Systems Design Engineer",
+    "a Passionate Gamer",
+    "a Dragon Enthusiast",
+    "a Lifelong Learner"
+  ];
+
+  const [currentPhrase, setCurrentPhrase] = useState([0]);
+  const [index, setIndex] = useState(0);
+
+  useEffect (() => {
+    const interval = setInterval(() => {
+      setIndex((prevIndex) => (prevIndex + 1) % phrases.length);
+    }, 6000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  useEffect (() => {
+    setCurrentPhrase(phrases[index]);
+  }, [index]);
+
   useEffect(() => {
     document.querySelectorAll('.contact-btn').forEach(button => {
       button.addEventListener('click', function() {
@@ -19,7 +42,7 @@ const Home = () => {
           <div className="text">
             <p>Hello,</p>
             <h1>
-              I'm <span>Nick Zheng</span>
+              I'm <span>{currentPhrase}</span>
             </h1>
             <p>I am a Systems Design Engineer student at the University of Waterloo</p>
           </div>
