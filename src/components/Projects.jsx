0e93734ef0d5a471efef35b4d8135335dc0a702b
Nick Zheng <nickzheng81@gmail.com>
2025-04-12 12:47:47 -0400

    updated name of projects file

---
+++ src/components/Projects.jsx
@@ -0,0 +1,13 @@
+import { React, useState } from "react";
+import "./css/projects.css";
+
+function Projects() {
+    return (
+        <div>
+            <h1>HELLO</h1>
+        </div>
+    );
+};
+
+export default Projects;
+
